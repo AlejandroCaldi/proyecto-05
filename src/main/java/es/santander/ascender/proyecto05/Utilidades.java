60128653df7854ff183ebed0401380519020cee5
curso07 <alejandro.cald@gmail.com>
2024-11-11 13:22:05 +0100

    Agergado de utiliddes de arrayValorMayor y arrayMenorValor

--- src/main/java/es/santander/ascender/proyecto05/Utilidades.java
+++ src/main/java/es/santander/ascender/proyecto05/Utilidades.java
@@ -2,4 +2,38 @@ package es.santander.ascender.proyecto05;
 
 public class Utilidades {
 
+    public double arrayValorMayor(double[] arregloInicial) {
+
+        double mayorValor = arregloInicial[0];
+
+        for (int i = 0; i<= arregloInicial.length -1;i++) {
+
+            if (arregloInicial[i] > mayorValor) {
+
+                mayorValor = arregloInicial[i];
+            }
+
+        }
+
+        return mayorValor;
+    }
+
+    public double arrayValorMenor(double[] arregloInicial) {
+
+        double menorValor = arregloInicial[0];
+
+        for (int i = 0; i<= arregloInicial.length -1;i++) {
+
+            if (arregloInicial[i] < menorValor) {
+
+                menorValor = arregloInicial[i];
+            }
+
+        }
+
+        return menorValor;
+    }
+
+
+
 }
