8baf2c8fd6bfbcacd01333bb8c33d7ba60910bc5
MACG <curso08@nomail.com>
2024-11-11 13:38:37 +0100

    Random

--- src/main/java/es/santander/ascender/proyecto05/Utilidades.java
+++ src/main/java/es/santander/ascender/proyecto05/Utilidades.java
@@ -1,5 +1,38 @@
 package es.santander.ascender.proyecto05;
 
+import java.util.Random;
+
 public class Utilidades {
 
+    public   int numeroRandom(int entre) {
+
+        
+        Random rn = new Random();
+        int numeroRandom = rn.nextInt(entre);
+
+        return numeroRandom;
+    }
+
+   public int [] listaNumerosRandom(int numeroiindice ,int entre ){
+
+      
+      int [] nuevalista = new int[numeroiindice];
+      for (int i = 0; i < nuevalista.length; i++) {
+
+        nuevalista[i] = numeroRandom(entre);
+      }
+
+      return nuevalista;
+    }
+
+           
+
+    
+    
+   
+        
+    
+
 }
+
+
