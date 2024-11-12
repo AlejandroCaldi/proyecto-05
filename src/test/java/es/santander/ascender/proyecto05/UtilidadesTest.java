12d6c268d81b0f59d00e4e983245f9a9f1ae5358
MACG <curso08@nomail.com>
2024-11-12 09:39:00 +0100

    test numeros negativos

--- src/test/java/es/santander/ascender/proyecto05/UtilidadesTest.java
+++ src/test/java/es/santander/ascender/proyecto05/UtilidadesTest.java
@@ -25,12 +25,23 @@ public class UtilidadesTest {
 
     @Test
     public void testNumeroRandom() {
-        int limite = 20;
+        int limite = -20;
         int[] teste = cut.listaNumerosRandom(5, limite);
         for (int i = 0; i < teste.length; i++) {
- 
-            assertTrue(teste[i] >= 0);
-            assertTrue(teste[i] <= limite);
+
+            if (limite < 0)
+
+            {
+                assertTrue(teste[i] >= limite);
+                assertTrue(teste[i]  >= 0);
+
+
+            }
+            else
+            { assertTrue(teste[i] >= 0);
+                assertTrue(teste[i] <= limite);
+            }
+           
 
 
         }
